70208c3cbf3ca13e7c7568f0f8191b4c862d1139
Dominik Fitz <dominikfitz@gmail.com>
2025-05-13 15:48:55 +0200

    Implementing a custom Hibernate listeners

---
+++ src/main/java/guru/springframework/creditcard/config/ListenerRegistrationConfig.java
@@ -0,0 +1,49 @@
+package guru.springframework.creditcard.config;
+
+import guru.springframework.creditcard.listeners.PostLoadListener;
+import guru.springframework.creditcard.listeners.PreInsertListener;
+import guru.springframework.creditcard.listeners.PreUpdateListener;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+@Configuration
+public class ListenerRegistrationConfig implements BeanPostProcessor {
+
+
+    private final PostLoadListener postLoadListener;
+    private final PreInsertListener preInsertListener;
+    private final PreUpdateListener preUpdateListener;
+
+    public ListenerRegistrationConfig(PostLoadListener postLoadListener, PreInsertListener preInsertListener, PreUpdateListener preUpdateListener) {
+        this.postLoadListener = postLoadListener;
+        this.preInsertListener = preInsertListener;
+        this.preUpdateListener = preUpdateListener;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+        if( bean instanceof LocalContainerEntityManagerFactoryBean) {
+            var lemf = (LocalContainerEntityManagerFactoryBean) bean;
+            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
+            EventListenerRegistry registry = sessionFactory.getServiceRegistry()
+                    .getService(EventListenerRegistry.class);
+
+            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
+            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
+            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
+        }
+
+        return bean;
+    }
+}
--- src/test/java/guru/springframework/creditcard/repositories/CreditCardRepositoryTest.java
+++ src/test/java/guru/springframework/creditcard/repositories/CreditCardRepositoryTest.java
@@ -32,29 +32,29 @@ class CreditCardRepositoryTest {
 
     @Test
     void testSaveAndStoreCreditCard() {
-        var creditCard = new CreditCard();
+        CreditCard creditCard = new CreditCard();
         creditCard.setCreditCardNumber(CREDIT_CARD);
         creditCard.setCvv("123");
         creditCard.setExpirationDate("12/2028");
 
         CreditCard savedCC = creditCardRepository.saveAndFlush(creditCard);
 
-        System.out.println("Getting CC from database" + creditCard.getCreditCardNumber());
+        System.out.println("Getting CC from database: " + savedCC.getCreditCardNumber());
 
         System.out.println("CC At Rest");
         System.out.println("CC Encrypted: " + encryptionService.encrpyt(CREDIT_CARD));
 
         Map<String, Object> dbRow = jdbcTemplate.queryForMap("SELECT * FROM credit_card " +
-                "WHERE id= " + savedCC.getId());
+                "WHERE id = " + savedCC.getId());
 
         String dbCardValue = (String) dbRow.get("credit_card_number");
+
         assertThat(savedCC.getCreditCardNumber()).isNotEqualTo(dbCardValue);
         assertThat(dbCardValue).isEqualTo(encryptionService.encrpyt(CREDIT_CARD));
 
         CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
 
         assertThat(savedCC.getCreditCardNumber()).isEqualTo(fetchedCC.getCreditCardNumber());
-
 
     }
 }
